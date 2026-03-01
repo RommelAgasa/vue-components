6916f8c2a42123203f4c58cbb9a4ba2317bc1504
_DevNote <rommelagasa102@gmail.com>
2026-03-01 15:38:55 +0800

    - component events

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,34 @@
 
   <ArticleComponent id="my-article" :title="'Article Title'" :likes="50" :isPublished="true"/>
   
-  <h3>AppComponent username {{ username }}</h3>
-  <ComponentC />
+  <!-- <h3>AppComponent username {{ username }}</h3>
+  <ComponentC /> -->
+
+  <button @click="showPopup = true">Show Popup</button>
+  <PopupComponent v-show="showPopup" @closePopup="closePopup"/>
+
+
 
 </template>
 
 <script>
 // mport GreetUser from './components/Greet.vue'
 import ArticleComponent from './components/Article.vue'
-import ComponentC from './components/ComponentC.vue';
+// import ComponentC from './components/ComponentC.vue';
+import PopupComponent from './components/Popup.vue';
 
 export default {
   name: 'App',
   components: {
     // GreetUser,
     ArticleComponent,
-    ComponentC
+    //ComponentC,
+    PopupComponent
   },
   data(){
     return {
-      name: 'Rommel'
+      name: 'Rommel',
+      showPopup: false
     }
   },
   // If property is only utilized in the immediate child component, you can specify provide as a function
@@ -37,6 +45,12 @@ export default {
   // provide: {
   //   username: 'rommel'
   // }
+  methods: {
+    closePopup(name) {
+      this.showPopup = false;
+      console.log(`Popup closed by ${name}`);
+    }
+  }
 }
 </script>
 
---
+++ src/components/Popup.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>This is a popup component</h2>
+        <button @click="$emit('closePopup', 'Kyla')">Close popup</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PopupComponent',
+        emits: ['closePopup']
+    }
+</script>
+
+<style scoped>
+
+</style>
