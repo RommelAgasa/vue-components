71d9a45b215ccd4793b243ee8b8e3ad0f333b18f
_DevNote <rommelagasa102@gmail.com>
2026-03-01 16:37:50 +0800

    - validating emits

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,14 +1,29 @@
 <template>
     <div>
         <h2>This is a popup component</h2>
-        <button @click="$emit('closePopup', 'Kyla')">Close popup</button>
+        <input type="text" v-model="name">
+        <button @click="$emit('closePopup', name)">Close popup</button>
     </div>
 </template>
 
 <script>
     export default {
         name: 'PopupComponent',
-        emits: ['closePopup']
+        // emits: ['closePopup'],
+        emits: {
+            closePopup: (name) => {
+                if (!name) {
+                    console.warn('Name is required to close the popup');
+                    return false;
+                }
+                return true;
+            }
+        },  
+        data() {
+            return {
+                name: ''
+            }
+        }
     }
 </script>
 
